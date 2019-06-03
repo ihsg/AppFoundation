c503875cbfc92fca4a9be4ab048505e3bb5f90bc
ihsg <hsg1040175759@gmail.com>
2019-06-03 17:12:10 +0800

    data binding: edittext

--- app/src/main/java/com/github/ihsg/appfoundation/sign/LoginVM.kt
+++ app/src/main/java/com/github/ihsg/appfoundation/sign/LoginVM.kt
@@ -2,14 +2,17 @@ package com.github.ihsg.appfoundation.sign
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.ihsg.appfoundation.common.util.LogUtil
 
 class LoginVM : ViewModel() {
     private var a = 0
     val textWelcome = MutableLiveData<String>()
     val imageUrl = MutableLiveData<String>()
+    val inputUserName = MutableLiveData<String>()
 
     fun onSubmit() {
         this.textWelcome.postValue("Welcome ${++a}")
         this.imageUrl.postValue("https://www.lvjinsuo.com/static/images/app/banner/depository_banner.png")
+        LogUtil.i("input user name: ${inputUserName.value}")
     }
 }
